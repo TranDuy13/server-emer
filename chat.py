49f5ac719907437c6d12b479e6fef1591b13eb55
Duy Tran <chiba@Duys-MacBook-Air.local>
2022-12-17 01:50:40 +0700

    AI

--- chat.py
+++ chat.py
@@ -27,23 +27,22 @@ model.eval()
 
 bot_name = "Ecommerce chatbot AI"
 
-    # sentence = "do you use credit cards?"
-    sentence = sys.argv[1]
-    sentence = tokenize(sentence)
-    X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
+sentence = 'sys.argv[1]'
+sentence = tokenize(sentence)
+X = bag_of_words(sentence, all_words)
+X = X.reshape(1, X.shape[0])
+X = torch.from_numpy(X).to(device)
 
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
+output = model(X)
+_, predicted = torch.max(output, dim=1)
 
-    tag = tags[predicted.item()]
+tag = tags[predicted.item()]
 
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand...")
+probs = torch.softmax(output, dim=1)
+prob = probs[0][predicted.item()]
+if prob.item() > 0.75:
+    for intent in intents['intents']:
+        if tag == intent["tag"]:
+            print(f"{bot_name}: {random.choice(intent['responses'])}")
+else:
+    print(f"{bot_name}: I do not understand...")
--- routes/aiRoute.js
+++ routes/aiRoute.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const {spawn} = require('child_process')
 router.get('/question',(req,res)=>{
 
-    const AI =spawn('python3',['chat.py',req.body.text])
+    const AI =spawn('python3',['./chat.py',req.body.text])
+
     AI.stdout.on('data',function(data){
         res.json(data.toString())
     })
